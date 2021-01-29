b78b2fc3ef00bd246dd8b55e32b03d3850ba9b97
SongJeongHun <song970124@gmail.com>
2021-01-29 17:11:13 +0900

    joinVCvalidationCheck

--- CalenDeck/ViewController/Login/UserJoinViewController.swift
+++ CalenDeck/ViewController/Login/UserJoinViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 import RxSwift
 class UserJoinViewController: UIViewController,ViewControllerBindableType {
     var viewModel:JoinViewModel!
+//    @IBOutlet weak var confirmComment:UILabel!
+    @IBOutlet weak var confirmButton:UIButton!
+    @IBOutlet weak var joinPanel:UIView!
     @IBOutlet weak var userID:UITextField!
     @IBOutlet weak var userPassword:UITextField!
     @IBOutlet weak var userPasswordConfirm:UITextField!
     @IBOutlet weak var userEmail:UITextField!
+    var userValidationList:[Bool] = [false,false,false,false]
+    lazy var userValidation = BehaviorSubject<[Bool]>(value: userValidationList)
     override func viewWillAppear(_ animated: Bool) {
         setUI()
         super.viewWillAppear(animated)
@@ -26,8 +31,12 @@ class UserJoinViewController: UIViewController,ViewControllerBindableType {
             .subscribe(onNext:{text in
                 if let ID = text{
                     if ID.count > 3{
+                        self.userValidationList[0] = true
+                        self.userValidation.onNext(self.userValidationList)
                         self.userID.layer.borderColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
                     }else{
+                        self.userValidationList[0] = false
+                        self.userValidation.onNext(self.userValidationList)
                         self.userID.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
                     }
                 }
@@ -37,21 +46,62 @@ class UserJoinViewController: UIViewController,ViewControllerBindableType {
             .subscribe(onNext:{pwd,pwdConfirm in
                 if let password = pwd, let passwordConfirm = pwdConfirm{
                     if password != passwordConfirm{
+                        self.userValidationList[2] = false
+                        self.userValidation.onNext(self.userValidationList)
                         self.userPasswordConfirm.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
                     }
                     if password.count > 6{
                         if password == passwordConfirm{
+                            self.userValidationList[2] = true
+                            self.userValidation.onNext(self.userValidationList)
                             self.userPasswordConfirm.layer.borderColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
                         }
+                        self.userValidationList[1] = true
+                        self.userValidation.onNext(self.userValidationList)
                         self.userPassword.layer.borderColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
                     }else{
+                        self.userValidationList[1] = false
+                        self.userValidation.onNext(self.userValidationList)
                         self.userPassword.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
                     }
                 }
             })
             .disposed(by: rx.disposeBag)
+        userEmail.rx.text
+            .subscribe(onNext:{text in
+                if let email = text{
+                    if self.isValidEmail(testStr: email){
+                        self.userValidationList[3] = true
+                        self.userValidation.onNext(self.userValidationList)
+                        self.userEmail.layer.borderColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+                    }else{
+                        self.userValidationList[3] = false
+                        self.userValidation.onNext(self.userValidationList)
+                        self.userEmail.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+                    }
+                }
+            })
+            .disposed(by: rx.disposeBag)
+        userValidation
+            .subscribe(onNext:{valid in
+                if !valid.contains(false){
+                    self.confirmButton.backgroundColor = #colorLiteral(red: 0.3569344282, green: 0.3205632567, blue: 0.8702369332, alpha: 1)
+                    self.confirmButton.isEnabled = true
+                }else{
+                    self.confirmButton.backgroundColor = #colorLiteral(red: 0.3569344282, green: 0.3205632567, blue: 0.8702369332, alpha: 0.4251080987)
+                    self.confirmButton.isEnabled = false
+                }
+            })
+            .disposed(by: rx.disposeBag)ç
+    }
+    func isValidEmail(testStr:String) -> Bool {
+          let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+          let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+          return emailTest.evaluate(with: testStr)
     }
     func setUI(){
+        confirmButton.layer.cornerRadius = 5.0
+        joinPanel.layer.cornerRadius = 5.0
         userPassword.isSecureTextEntry = true
         userPasswordConfirm.isSecureTextEntry = true
         userPassword.layer.cornerRadius = 5.0
